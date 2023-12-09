40a07b193db7e3a6e7ccbc7dee15f0d13a790235
Ian <iandkaneshiro@gmail.com>
2023-12-09 10:43:07 -0800

    added enemeies

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy } from "./enemies.js";
 
 window.addEventListener("load", function () {
   const canvas = document.getElementById("canvas1");
@@ -18,16 +19,39 @@ window.addEventListener("load", function () {
       this.background = new Background(this);
       this.player = new Player(this);
       this.input = new InputHandler();
+      this.enemies = [];
+      this.enemyTimer = 0;
+      this.enemyInterval = 1000;
     }
 
     update(deltaTime) {
       this.background.update();
       this.player.update(this.input.keys, deltaTime);
+      // handleEnemies
+
+      if (this.enemyTimer > this.enemyInterval) {
+        this.addEnemy();
+        this.enemyTimer = 0;
+      } else {
+        this.enemyTimer += deltaTime;
+      }
+      this.enemies.forEach((e) => {
+        e.update(deltaTime);
+        if (e.markedForDeletion)
+          this.enemies.splice(this.enemies.indexOf(e), 1);
+      });
     }
 
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
+      this.enemies.forEach((e) => {
+        e.draw(context);
+      });
+    }
+    addEnemy() {
+      this.enemies.push(new FlyingEnemy(this));
+      console.log(this.enemies);
     }
   }
 
